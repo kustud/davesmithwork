e316387e393fc8422b0f5ed512f3c2fcb53d9cc4
davidsmith_ow <david.smith@affiliate.oliverwyman.com>
2020-02-10 13:44:44 +0000

    Update to main image section (again).

--- js/somejavascriptstuff.js
+++ js/somejavascriptstuff.js
@@ -4,28 +4,32 @@ var x = 0;
 var y = 0;
 var friction = 1 / 30;
 
-function moveBackground() {
-    x += (lFollowX - x) * friction;
-    y += (lFollowY - y) * friction;
+$( document ).ready(function() {
 
-    translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+    function moveBackground() {
+        x += (lFollowX - x) * friction;
+        y += (lFollowY - y) * friction;
 
-    $('.bg').css({
-        '-webit-transform': translate,
-        '-moz-transform': translate,
-        'transform': translate
+        translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+        $('.bg').css({
+            '-webit-transform': translate,
+            '-moz-transform': translate,
+            'transform': translate
+        });
+
+        window.requestAnimationFrame(moveBackground);
+    }
+
+    $(window).on('mousemove click', function(e) {
+
+        var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+        var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+        lFollowX = (20 * lMouseX) / 100;
+        lFollowY = (10 * lMouseY) / 100;
+
     });
 
-    window.requestAnimationFrame(moveBackground);
-}
-
-$(window).on('mousemove click', function(e) {
-
-    var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-    var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
-    lFollowX = (20 * lMouseX) / 100;
-    lFollowY = (10 * lMouseY) / 100;
+    moveBackground();
 
 });
-
-moveBackground();
